ce2ad29e1d5bfbfb43c5ab67babd403fe4632d09
Shaii Ong <monsteronfire@gmail.com>
2017-09-24 11:22:00 +0800

    [WIP] Add webpack configs

---
+++ webpack.config.babel.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: __dirname +  '/dist',
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.(js)$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: [ 'style-loader!css-loader' ] }
+    ]
+  }
+};
