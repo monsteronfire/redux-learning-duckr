2a2848f6c454676349620e6c0718724d251ac909
Shaii Ong <monsteronfire@gmail.com>
2017-10-01 22:54:35 +0800

    Add basic styling

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,9 +1,11 @@
 import React, { PropTypes } from 'react';
+import {} from './styles.css';
 
 function Home (props) {
   return (
     <div>
-      Home
+      <p>{'Duckr'}</p>
+      <p>{'The real time, cloud-based, modular, scalable, growth hack, social platform in the cloud'}</p>
     </div>
   )
 }
